1813ed20f798e1919d4b85b43d15a3a72c620d38
mojisilva <mojiasilva@gmail.com>
2022-04-06 20:20:30 -0300

    updating resouces to responsive layout

--- script/script.js
+++ script/script.js
@@ -1,13 +1,30 @@
 var swiper = new Swiper(".mySwiper", {
   loop: true,
-  slidesPerView:5,
   slidesPerGroup:5,
-  spaceBetween: 10,
-  navigation: {
+    navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
   scrollbar: {
     el: '.swiper-scrollbar',
   } ,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    700: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1000: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+  },
 });
+
+function abrir(){
+  document.getElementById('popup').style.display = 'block';
+
+}
